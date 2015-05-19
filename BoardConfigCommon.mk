b556290a0cbdc37b01cd0c6be13a0542c9efc2ca
Brawn_Sg <bhavy.singhal3@gmail.com>
2015-05-19 15:38:13 +0530

    Revert "Revert "upload""
    This reverts commit 35609d8c4b3d6958829cd21c1c032029ceef4bcd.

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,154 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/xiaomi/mocha/include
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a15
+TARGET_CPU_SMP := true
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+
+# Board
+TARGET_BOARD_PLATFORM := tegra
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/nvidia/shieldtablet
+TARGET_KERNEL_CONFIG := cyanogenmod_shieldtablet_defconfig
+BOARD_KERNEL_CMDLINE := "androidboot.selinux=permissive"
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 671088640
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13742637056
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/Xiaomi/mocha/kernel
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --dt device/Xiaomi/mocha/dt.img --tags_offset 0x00000100
+#BOARD_KERNEL_SEPARATED_DT := true
+
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+OARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/Xiaomi/mocha/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/Xiaomi/mocha/bluetooth/vnd_mocha.txt
+
+# Graphics
+USE_OPENGL_RENDERER := true
+BOARD_DISABLE_TRIPLE_BUFFERED_DISPLAY_SURFACES := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Per-application sizes for shader cache
+MAX_EGL_CACHE_SIZE := 4194304
+MAX_EGL_CACHE_ENTRY_SIZE := 262144
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+#BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/backlight/pwm-backlight/brightness\"
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/Xiaomi/mocha/recovery/recovery_keys.c
+TARGET_RECOVERY_FSTAB := device/Xiaomi/mocha/recovery.fstab
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_RECOVERY_SWIPE := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+#RECOVERY_TOUCHSCREEN_FLIP_Y := true
+#RECOVERY_TOUCHSCREEN_FLIP_X := true
+TARGET_PREBUILT_RECOVERY_KERNEL := device/Xiaomi/mocha/kernel
+BOARD_MKRECOVERYIMG_ARGS := --dt device/Xiaomi/mocha/dt.img
+PRODUCT_COPY_FILE += \
+device/Xiaomi/mocha/rootdir/sbin/e2fsck_static:recovery/root/sbin/e2fsck_static
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_MODULE_ARG := "iface_name=wlan0"
+WIFI_DRIVER_MODULE_NAME := "bcmdhd"
+BOARD_HARDWARE_CLASS := device/Xiaomi/mocha/cmhw/
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/xiaomi/mocha/sepolicy
+BOARD_SEPOLICY_UNION += \
+	te_macros \
+	app.te \
+	bluetooth.te \
+	bootanim.te \
+	cvc.te \
+	device.te \
+	domain.te \
+	drmserver.te \
+	file_contexts \
+	file.te \
+	genfs_contexts \
+	gpload.te \
+	gpsd.te \
+	hostapd.te \
+	installd.te \
+	mediaserver.te \
+	netd.te \
+	platform_app.te \
+	property_contexts \
+	property.te \
+	raydium.te \
+	recovery.te \
+	service.te \
+	service_contexts \
+	set_hwui.te \
+	shell.te \
+	surfaceflinger.te \
+	system_app.te \
+	system_server.te \
+	tee.te \
+	ueventd.te \
+	untrusted_app.te \
+	usb.te \
+	ussrd.te \
+	ussr_setup.te \
+	vold.te \
+	wifi_loader.te \
+	wpa.te \
+	zygote.te \
+	healthd.te
+
